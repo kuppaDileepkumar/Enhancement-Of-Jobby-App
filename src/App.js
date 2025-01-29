731bfdd853b0af0573a6adecc8f34b7d0a2b6453
kuppaDileepkumar <135305572+kuppaDileepkumar@users.noreply.github.com>
2025-01-29 19:58:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import {Route, Switch, Redirect} from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom';
 
-import LoginForm from './components/LoginForm'
-import Home from './components/Home'
-import Jobs from './components/Jobs'
-import JobItemDetails from './components/JobItemDetails'
-import NotFound from './components/NotFound'
-import ProtectedRoute from './components/ProtectedRoute'
+import LoginForm from './components/LoginForm';
+import Home from './components/Home';
+import Jobs from './components/Jobs';
+import JobItemDetails from './components/JobItemDetails';
+import NotFound from './components/NotFound';
+import ProtectedRoute from './components/ProtectedRoute';
 
-import './App.css'
+import './App.css';
 
 const App = () => (
   <>
-    <Switch>
-      <Route exact path="/login" component={LoginForm} />
-      <ProtectedRoute exact path="/" component={Home} />
-      <ProtectedRoute exact path="/jobs" component={Jobs} />
-      <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
-      <Route path="/not-found" component={NotFound} />
-      <Redirect to="not-found" />
-    </Switch>
+    <Routes>
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+      <Route path="/jobs" element={<ProtectedRoute><Jobs /></ProtectedRoute>} />
+      <Route path="/jobs/:id" element={<ProtectedRoute><JobItemDetails /></ProtectedRoute>} />
+      <Route path="/not-found" element={<NotFound />} />
+      <Route path="*" element={<Navigate to="/not-found" />} />
+    </Routes>
   </>
-)
+);
 
-export default App
+export default App;
