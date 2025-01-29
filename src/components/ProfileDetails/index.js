ef584ff874dec5af850322b1970b2b7b69da47a1
kuppaDileepkumar <135305572+kuppaDileepkumar@users.noreply.github.com>
2025-01-29 20:11:52 +0530

    Update index.js

--- src/components/ProfileDetails/index.js
+++ src/components/ProfileDetails/index.js
@@ -1,5 +1,6 @@
 import {Component} from 'react'
-import Loader from 'react-loader-spinner'
+import { TailSpin } from 'react-loader-spinner'
+
 import Cookies from 'js-cookie'
 import './index.css'
 
